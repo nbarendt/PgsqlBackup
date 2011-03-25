9c0e25cee98a1b8d260e4bf39c6cbab72e366ecb
Nick Barendt <nickbarendt@gmail.com>
2011-03-25 16:56:29 -0400

    simplifying tests

--- tests/archivepgsql_test/test_option_parsing.py
+++ tests/archivepgsql_test/test_option_parsing.py
@@ -3,28 +3,31 @@ from bbpgsql.option_parser import parse_args
 from testfixtures import TempDirectory
 
 
-class Test_CommandLineOptionParsing(TestCase):
+class Test_CommandLineOptionParsing_Defaults(TestCase):
+    def setUp(self):
+        self.options, self.args = parse_args(args=[])
 
     def test_can_parse_empty_command_line(self):
-        options, args = parse_args(args=[])
-        self.assertTrue(options)
-
-    def test_uses_etc_config_file_by_default(self):
-        options, args = parse_args(args=[])
-        self.assertEqual('/etc/bbpgsql.ini', options.config_file)
-
-    def test_override_config_file(self):
-        config_path = '/tmp/blah/blah/bbpgsql.ini'
-        options, args = parse_args(args=['--config', config_path])
-        self.assertEqual(config_path, options.config_file)
+        self.assertTrue(self.options)
 
     def test_not_dry_run_by_default(self):
-        options, args = parse_args(args=[])
-        self.assertFalse(options.dry_run)
+        self.assertFalse(self.options.dry_run)
+
+    def test_uses_etc_config_file_by_default(self):
+        self.assertEqual('/etc/bbpgsql.ini', self.options.config_file)
+
+
+class Test_CommandLineOptionParsing_With_Options(TestCase):
+    def setUp(self):
+        self.config_path = '/tmp/blah/blah/bbpgsql.ini'
+        args = ['--dry-run', '--config', self.config_path]
+        self.options, self.args = parse_args(args=args)
+
+    def test_override_config_file(self):
+        self.assertEqual(self.config_path, self.options.config_file)
 
     def test_can_execute_dry_run(self):
-        options, args = parse_args(args=['--dry-run'])
-        self.assertTrue(options.dry_run)
+        self.assertTrue(self.options.dry_run)
 
 
 class Test_OptionParsing_and_Validation(TestCase):
