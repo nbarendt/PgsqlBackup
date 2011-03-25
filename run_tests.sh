1268478fea232efe722a8d4b0d1fbd6728d26875
Nick Barendt <nickbarendt@gmail.com>
2011-03-25 13:43:05 -0400

    simplified run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-find tests -name "*.py" | xargs pep8
-find tests -name "*.py" | xargs pyflakes
-
-find bbpgsql -name "*.py" | xargs pep8
-find bbpgsql -name "*.py" | xargs pyflakes
+find bbpgsql tests -name "*.py" | xargs pyflakes
+pep8 bbpgsql tests
 
 nosetests
