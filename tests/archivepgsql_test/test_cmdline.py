72461935e74b43099d15d1872886256c71d5f4c2
Nick Barendt <nickbarendt@gmail.com>
2011-03-25 10:33:09 -0400

    added testfixture tempdirectory and config
    archivepgsql now looks for a configuration file (defaults to /etc/bbpgsql.ini)
      and supports a command-line override to specify a particular configuration file
    the command-line override for the configuration file is primarily intended for testing, but may have use in production

--- tests/archivepgsql_test/test_cmdline.py
+++ tests/archivepgsql_test/test_cmdline.py
@@ -3,16 +3,31 @@ from unittest import TestCase
 from subprocess import check_call
 import os
 from copy import deepcopy
+from testfixtures import TempDirectory
 
 class Test_archivepgsql_BasicCommandLineOperation(object):
     ARCHIVEPGSQL_PATH=os.path.join('PgsqlBackup', 'cmdline_scripts')
-    cmd = 'archivepgsql'
+    CONFIG_FILE = 'config.ini' 
+    exe_script = 'archivepgsql'
 
     def setup(self):
+        self.setup_environment()
+        self.setup_config()
+        self.cmd = [self.exe_script, '--config', self.config_path]
+
+    def setup_environment(self):
         self.env = deepcopy(os.environ)
-        self.env['PATH'] = ''.join([self.env['PATH'],
+        self.env['PATH'] = ''.join([
+            self.env['PATH'],
             ':',
             self.ARCHIVEPGSQL_PATH])
+        self.tempdir = TempDirectory()
+
+    def setup_config(self):
+        self.config_path = self.tempdir.write(self.CONFIG_FILE, '')
+
+    def teardown(self):
+        self.tempdir.cleanup()
 
     def test_can_execute_archivepgsql(self):
-       check_call([self.cmd], env=self.env)
+        check_call(self.cmd, env=self.env)
