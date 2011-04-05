58617e210d81894c2d7de56c2ca58e6c134aa54f
Mark Dohring <markdohring@gmail.com>
2011-04-05 15:31:05 -0400

    changed tabs to spaces, deleted excess blank lines

--- tests/PgsqlBackup_tests.py
+++ tests/PgsqlBackup_tests.py
@@ -1,19 +1,17 @@
-from nose.tools import *
-import bbpgsql
-#from tar_create import tar_create
 from bbpgsql.file_system_archive import create_file_system_archive
 
 
 def setup():
-	print "SETUP!"
+    print "SETUP!"
+
 
 def teardown():
-	print "TEAR DOWN!"
+    print "TEAR DOWN!"
+
 
 def test_basic():
-	print "I RAN!"
+    print "I RAN!"
+
 
 def test_create_file_system_archive():
-	create_file_system_archive('archive.tar')
-	
-
+    create_file_system_archive('archive.tar')
