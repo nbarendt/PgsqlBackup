6c052f76fe41342183a5be328551b0da393e8955
Nick Barendt <nickbarendt@gmail.com>
2011-04-04 21:45:14 -0400

    initial sketch of a memory-resident repository
    can commit a file, given a tag and an optional message

---
+++ bbpgsql/repository.py
@@ -0,0 +1,68 @@
+from re import match
+
+class DuplicateTagError(Exception):
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __str__(self):
+        msg = 'DuplicateTagError: the tag "{0}" already exists in' \
+              ' the repository'.format(self.tag)
+        return msg
+
+class BBCommit(object):
+    def __init__(self, tag, contents, message):
+        self.tag = tag
+        self.contents = contents
+        self.message = message
+
+    def get_to_filename(self, filename):
+        open(filename, 'wb').write(self.contents)
+
+ 
+class BBRepository(object):
+    def __init__(self):
+        self.data = {}
+
+    def _get_commit_by_tag(self, tag):
+        return BBCommit(tag, self.data[tag]['data'],
+            self.data[tag]['message'])
+
+    def __iter__(self):
+        for tag in self._sorted_tags():
+            yield self._get_commit_by_tag(tag)
+
+    def __getitem__(self, tag):
+        return self._get_commit_by_tag(tag)
+
+    def _sorted_tags(self):
+        return sorted(self.data.keys())
+
+    def _tag_is_unique(self, tag):
+        if tag in self.data:
+            raise DuplicateTagError(tag)
+
+    def _message_is_legal(self, message):
+        message_regex = r'^[a-zA-Z0-9\_]*$'
+        if message and not match(message_regex, message):
+            raise Exception('Message "{0}" does not match regex "{1}"'.format(
+                message, message_regex))        
+
+    def create_commit_from_filename(self, tag, filename, message=None):
+        message = message or ''
+        self._tag_is_unique(tag)
+        self._message_is_legal(message)
+        self.data[tag] = dict(data=open(filename, 'rb').read(),
+                                message=message)
+
+    def delete_commits_before(self, new_oldest_commit):
+        new_oldest_tag = new_oldest_commit.tag
+        tags_to_delete = [k for k in self._sorted_tags() if k < new_oldest_tag]
+        for tag in tags_to_delete:
+            del self.data[tag] 
+
+    def get_commit_before(self, commit_N):
+        tags = self._sorted_tags()
+        N_minus_1 = tags.index(commit_N.tag) - 1
+        if N_minus_1 < 0:
+            raise ValueError()
+        return self[tags[N_minus_1]]
---
+++ tests/repository_test/test_memory_repository.py
@@ -0,0 +1,83 @@
+from unittest import TestCase
+import os
+from testfixtures import TempDirectory
+from bbpgsql.repository import BBRepository, DuplicateTagError
+
+class Test_DuplicateTagError(TestCase):
+    def test_str_output(self):
+        expected = 'DuplicateTagError: the tag "badtag" already exists in the repository'
+        self.assertEqual(expected, str(DuplicateTagError('badtag')))
+
+class Test_Basic_Rpository_Operations_On_MemoryRepository(TestCase):
+    def setUp(self):
+        self.tempdir = TempDirectory()
+        self.repo = BBRepository()
+        self.file1 = self.tempdir.write('self.file1', 'some contents')
+        self.file2 = self.tempdir.write('file2', 'some other contents')
+
+    def tearDown(self):
+        self.tempdir.cleanup()
+
+    def commit_file1(self, tag, message=None):
+        self.repo.create_commit_from_filename(tag, self.file1, message) 
+
+    def commit_file2(self, tag, message=None):
+        self.repo.create_commit_from_filename(tag, self.file2, message) 
+
+    def test_can_commit_files_to_repository(self):
+        self.commit_file1('some tag')
+
+    def test_repo_is_empty_to_start(self):
+        self.assertEqual([], [c for c in self.repo]) 
+
+    def test_can_commit_files_and_list_commits(self):
+        self.commit_file1('some tag')
+        self.assertEqual(['some tag'], [c.tag for c in self.repo]) 
+
+    def test_can_commit_and_retrieve_contents(self):
+        self.commit_file1('some tag')
+        commit = self.repo['some tag']
+        commit.get_to_filename(self.file2)
+        self.assertEqual('some contents', open(self.file2, 'rb').read()) 
+    
+    def test_tags_are_unique(self):
+        self.commit_file1('some tag')
+        def will_raise_DuplicateTagError():
+            self.repo.create_commit_from_filename('some tag', self.file1) 
+        self.assertRaises(DuplicateTagError, will_raise_DuplicateTagError)
+
+    def test_can_get_commit_before_a_given_commit(self):
+        self.commit_file1('a')
+        self.commit_file1('b')
+        commit_b = self.repo['b']
+        self.assertEqual('a', self.repo.get_commit_before(commit_b).tag)
+
+    def test_commit_before_first_raises_ValueError(self):
+        self.commit_file1('a')
+        def will_raise_ValueError():
+            self.repo.get_commit_before(self.repo['a'])
+        self.assertRaises(ValueError, will_raise_ValueError)
+
+    def test_commits_are_sorted(self):
+        self.commit_file1('c')
+        self.commit_file1('a')
+        self.commit_file1('b')
+        self.assertEqual(['a', 'b', 'c'], [c.tag for c in self.repo]) 
+
+    def test_can_delete_commits_before_a_specified_commit(self):
+        self.commit_file1('a')
+        self.commit_file1('b')
+        self.commit_file1('c')
+        self.repo.delete_commits_before(self.repo['c'])
+        self.assertEqual(['c'], [c.tag for c in self.repo]) 
+
+    def test_can_store_and_retrieve_message_with_commit(self):
+        message = 'some_extra_data'
+        self.commit_file1('a', message)
+        commit = self.repo['a']
+        self.assertEqual(message, commit.message)
+
+    def test_message_character_set_limited_to_alphanumeric_and_underscore(self):
+        def will_raise_Exception():
+            self.commit_file1('a', message,'some illegal data')
+        self.assertRaises(Exception, will_raise_Exception)
