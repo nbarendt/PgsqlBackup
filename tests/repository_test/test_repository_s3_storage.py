800a2e99320b7d337cf924b0618b35e081d8dc8b
Nick Barendt <nickbarendt@gmail.com>
2011-04-06 11:05:10 -0400

    split out repository testing into 3 separate tests
    one test per backing storage

--- tests/repository_test/test_repository_s3_storage.py
+++ tests/repository_test/test_repository_s3_storage.py
@@ -1,7 +1,3 @@
-from unittest import TestCase
-from bbpgsql.repository_exceptions import DuplicateTagError
-from bbpgsql.repository_storage_test import MemoryCommitStorage
-from bbpgsql.repository_storage_test import FilesystemCommitStorage
 from bbpgsql.repository_storage_s3 import S3CommitStorage
 from ConfigParser import SafeConfigParser
 from boto import connect_s3
@@ -10,32 +6,6 @@ from tests.repository_test.repository_test_skeleton import (
     Skeleton_Repository_Operations_With_SpecificCommitStorage
     )
 
-
-class Test_Repository_Operations_With_MemoryCommitStorage(
-    Skeleton_Repository_Operations_With_SpecificCommitStorage):
-    __test__ = True
-
-    def setUp(self):
-        self.setup_tempdir()
-        self.store = MemoryCommitStorage()
-        self.setup_repository()
-
-    def tearDown(self):
-        self.teardown_tempdir()
-
-
-class Test_Repository_Operations_With_FileSystemCommitStore(
-    Skeleton_Repository_Operations_With_SpecificCommitStorage):
-    __test__ = True
-
-    def setUp(self):
-        self.setup_tempdir()
-        self.repo_path = self.tempdir.makedir('repo')
-        self.store = FilesystemCommitStorage(self.repo_path)
-        self.setup_repository()
-
-    def tearDown(self):
-        self.teardown_tempdir()
 
 class Test_Repository_Operations_With_S3CommitStorage(
     Skeleton_Repository_Operations_With_SpecificCommitStorage):
