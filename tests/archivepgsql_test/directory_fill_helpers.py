6143c97cab86d9dc4b83791b1ca24184341f4866
Mark Dohring <markdohring@gmail.com>
2011-04-12 16:35:40 -0400

    Create some new helpers to fill directories with test files.

---
+++ tests/archivepgsql_test/directory_fill_helpers.py
@@ -0,0 +1,19 @@
+def generate_filenames(number):
+    if number < 1 or number > 10:
+        raise ValueError('Number of files out of range.  1 to 10 is valid')
+    return [''.join(['file', str(index)]) for index in xrange(0, number)]
+
+
+def generate_file_contents(number):
+    if number < 1 or number > 10:
+        raise ValueError('Number of files out of range.  1 to 10 is valid')
+    return [str(index) * index for index in xrange(0, number)]
+
+def write_files(dir, files):
+    for file in files:
+        dir.write(file[0], file[1])
+
+def create_files(dir, number):
+    fileNames = generate_filenames(number)
+    fileContents = generate_file_contents(number)
+    write_files(dir, zip(fileNames, fileContents))
---
+++ tests/archivepgsql_test/test_directory_fill_helpers.py
@@ -0,0 +1,64 @@
+from unittest import TestCase
+from nose.plugins.skip import SkipTest
+from testfixtures import TempDirectory
+from directory_fill_helpers import generate_filenames
+from directory_fill_helpers import generate_file_contents
+from directory_fill_helpers import write_files
+from directory_fill_helpers import create_files
+
+class Test_directory_fill_helpers(TestCase):
+    def setUp(self):
+        self.testDir = TempDirectory()
+
+    def tearDown(self):
+        self.testDir.cleanup()
+
+    def test_create_files(self):
+        raise SkipTest
+        self.fail()
+
+    def test_generate_filenames_one_file(self):
+        fileNames = generate_filenames(1)
+        self.assertTrue(fileNames == ['file0'])
+
+    def test_generate_filenames_number_out_of_range(self):
+        self.assertRaises(ValueError, generate_filenames, 0)
+        self.assertRaises(ValueError, generate_filenames, 11)
+
+    def test_generate_filenames_two_files(self):
+        fileNames = generate_filenames(2)
+        self.assertTrue(fileNames == ['file0', 'file1'])
+
+    def test_genertate_file_contents_one_file(self):
+        fileContents = generate_file_contents(1)
+        self.assertTrue(fileContents == [''])
+
+    def test_generate_file_contents_number_out_of_range(self):
+        self.assertRaises(ValueError, generate_file_contents, 0)
+        self.assertRaises(ValueError, generate_file_contents, 11)
+
+    def test_generate_file_contents_two_files(self):
+        fileContents = generate_file_contents(2)
+        self.assertTrue(fileContents == ['', '1'])
+
+    def test_write_test_files_one_file(self):
+        fileNames = generate_filenames(1)
+        fileContents = generate_file_contents(1)
+        files = zip(fileNames, fileContents)
+        write_files(self.testDir, files)
+        self.testDir.check(*fileNames)
+        
+    def test_write_test_files_ten_files(self):
+        fileNames = generate_filenames(10)
+        fileContents = generate_file_contents(10)
+        files = zip(fileNames, fileContents)
+        write_files(self.testDir, files)
+        self.testDir.check(*fileNames)
+
+    def test_create_files_one_file(self):
+        create_files(self.testDir, 1)
+        self.testDir.check(*generate_filenames(1))
+
+    def test_create_files_ten_files(self):
+        create_files(self.testDir, 10)
+        self.testDir.check(*generate_filenames(10))
