ea66ca871dcf9351567021e9723e195f3e1f99a9
Mark Dohring <markdohring@gmail.com>
2011-04-26 14:38:38 -0400

    cosmetic cleanup

--- bbpgsql/configuration/repository.py
+++ bbpgsql/configuration/repository.py
@@ -6,5 +6,5 @@ from bbpgsql.configuration.repository_storage import (
 
 def get_WAL_repository(config):
     repository_type = 'WAL storage'
-    commit_storage = get_repository_storage_from_config(config,repository_type)
+    commit_storage = get_repository_storage_from_config(config, repository_type)
     return BBRepository(commit_storage)
