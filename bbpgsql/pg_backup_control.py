b3cd2261235ab9ea84d3aa754702ea4a5de51ead
Nick Barendt <nickbarendt@gmail.com>
2011-05-04 10:24:20 -0400

    query version of psql running

--- bbpgsql/pg_backup_control.py
+++ bbpgsql/pg_backup_control.py
@@ -1,4 +1,20 @@
 from subprocess import check_output
+from re import match
+
+def get_pg_version_output():
+    CMD = []
+    CMD.append('psql')
+    CMD.append('--version')
+    return check_output(CMD).strip()
+    
+def pg_get_version():
+    version_string = get_pg_version_output().strip().split('\n')[0]
+    version_regex = r'^psql\s\(PostgreSQL\)\s(\d+)\.(\d+)\.(\d+)$' 
+    match_results = match(version_regex, version_string)
+    if match_results:
+        return '{0}.{1}'.format(match_results.group(1), match_results.group(2))
+    else:
+        raise Exception('Unable to determine psql version')
 
 def pg_start_backup(label):
     cmd_prefix = "SELECT pg_xlogfile_name(pg_start_backup('"
--- tests/archivepgsql_test/test_pg_backup_control.py
+++ tests/archivepgsql_test/test_pg_backup_control.py
@@ -1,8 +1,60 @@
 from unittest import TestCase
 from mock import patch
 from bbpgsql.pg_backup_control import pg_start_backup
+from bbpgsql.pg_backup_control import pg_get_version
+from bbpgsql.pg_backup_control import get_pg_version_output
 from bbpgsql.pg_backup_control import pg_stop_backup
 from bbpgsql.pg_backup_control import wal_name_cleanup
+
+SAMPLE_8_3_12_VERSION_OUTPUT = """
+psql (PostgreSQL) 8.3.12
+contains support for command-line editing
+"""
+
+SAMPLE_8_3_7_VERSION_OUTPUT = """
+psql (PostgreSQL) 8.3.7
+contains support for command-line editing
+"""
+
+SAMPLE_8_4_7_VERSION_OUTPUT = """
+psql (PostgreSQL) 8.4.7
+contains support for command-line editing
+"""
+
+# this is entirely made up
+SAMPLE_UNPARSABLE_VERSION_OUTPUT = """
+psql (PSQL) 8.4a.7-rc1
+contains support for command-line editing
+"""
+
+@patch('bbpgsql.pg_backup_control.get_pg_version_output')
+class Test_pg_get_version(TestCase):
+    def test_parses_version_for_8_3_12_properly(self, mock_query_version): 
+        mock_query_version.return_value = SAMPLE_8_3_12_VERSION_OUTPUT 
+        self.assertEqual('8.3', pg_get_version())
+
+    def test_parses_version_for_8_3_7_properly(self, mock_query_version): 
+        mock_query_version.return_value = SAMPLE_8_3_7_VERSION_OUTPUT 
+        self.assertEqual('8.3', pg_get_version())
+
+    def test_parses_version_for_8_4_7_properly(self, mock_query_version): 
+        mock_query_version.return_value = SAMPLE_8_4_7_VERSION_OUTPUT 
+        self.assertEqual('8.4', pg_get_version())
+
+    def test_raises_exception_on_unparsable_version(self, mock_query_version):
+        mock_query_version.return_value = SAMPLE_UNPARSABLE_VERSION_OUTPUT 
+
+        def raises_Exception():
+            pg_get_version()
+
+        self.assertRaises(Exception, raises_Exception)
+
+@patch('bbpgsql.pg_backup_control.check_output')
+class Test_get_pg_version_output(TestCase):
+    def test_calls_psql_version(self, mock_check_output):
+        get_pg_version_output()
+        mock_check_output.assert_called_with(['psql', '--version'])
+
 
 @patch('bbpgsql.pg_backup_control.check_output')
 class Test_pg_start_backup(TestCase):
