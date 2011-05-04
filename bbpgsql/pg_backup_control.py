741f00b5aa5df6075b0e4d1cef359e0469c3ecc9
Nick Barendt <nickbarendt@gmail.com>
2011-05-04 11:00:06 -0400

    added pg_start_backup() support for pgsql 8.3
    and support for both unparsable pgsql versions and unsupported versions

--- bbpgsql/pg_backup_control.py
+++ bbpgsql/pg_backup_control.py
@@ -1,6 +1,21 @@
 from subprocess import check_output
 from re import match
 
+
+PGSQL_START_BACKUP_SQL_COMMAND_STRING = {
+    '8.3': "SELECT pg_xlogfile_name(pg_start_backup('{0}'));",
+    '8.4': "SELECT pg_xlogfile_name(pg_start_backup('{0}', true));",
+}
+
+PGSQL_SUPPORTED_VERSIONS = PGSQL_START_BACKUP_SQL_COMMAND_STRING.keys()
+
+class UnsupportedPostgresVersionError(Exception):
+    def __init__(self, version):
+        self.msg = 'Unsupported Version Of Postgresql: {0}'.format(version)
+
+    def __str__(self):
+        return self.msg
+
 def get_pg_version_output():
     CMD = []
     CMD.append('psql')
@@ -12,14 +27,18 @@ def pg_get_version():
     version_regex = r'^psql\s\(PostgreSQL\)\s(\d+)\.(\d+)\.(\d+)$' 
     match_results = match(version_regex, version_string)
     if match_results:
-        return '{0}.{1}'.format(match_results.group(1), match_results.group(2))
+        version = '{0}.{1}'.format(match_results.group(1),
+            match_results.group(2))
+        if version not in PGSQL_SUPPORTED_VERSIONS:
+            raise UnsupportedPostgresVersionError(version)
+        return version
     else:
         raise Exception('Unable to determine psql version')
 
 def pg_start_backup(label):
-    cmd_prefix = "SELECT pg_xlogfile_name(pg_start_backup('"
-    cmd_suffix = "', true));"
-    sql_cmd = ''.join([cmd_prefix, label, cmd_suffix])
+    pg_version = pg_get_version()
+    sql_cmd = PGSQL_START_BACKUP_SQL_COMMAND_STRING[pg_version].format(label)
+
     CMD = []
     CMD.append('psql') # what the's path?
     CMD.append('postgres') # need the right database
--- tests/archivepgsql_test/test_pg_backup_control.py
+++ tests/archivepgsql_test/test_pg_backup_control.py
@@ -3,6 +3,7 @@ from mock import patch
 from bbpgsql.pg_backup_control import pg_start_backup
 from bbpgsql.pg_backup_control import pg_get_version
 from bbpgsql.pg_backup_control import get_pg_version_output
+from bbpgsql.pg_backup_control import UnsupportedPostgresVersionError
 from bbpgsql.pg_backup_control import pg_stop_backup
 from bbpgsql.pg_backup_control import wal_name_cleanup
 
@@ -27,6 +28,10 @@ psql (PSQL) 8.4a.7-rc1
 contains support for command-line editing
 """
 
+SAMPLE_UNSUPPORTED_VERSION = """
+psql (PostgreSQL) 7.4.25
+"""
+
 @patch('bbpgsql.pg_backup_control.get_pg_version_output')
 class Test_pg_get_version(TestCase):
     def test_parses_version_for_8_3_12_properly(self, mock_query_version): 
@@ -49,6 +54,23 @@ class Test_pg_get_version(TestCase):
 
         self.assertRaises(Exception, raises_Exception)
 
+    def test_raises_UnsupportedPostgresVersionError(self, mock_query_version):
+        mock_query_version.return_value = SAMPLE_UNSUPPORTED_VERSION 
+
+        def raises_Exception():
+            pg_get_version()
+        self.assertRaises(UnsupportedPostgresVersionError, raises_Exception) 
+
+    def test_UnsupportedPostgresVersionError_has_expected_msg(self,
+        mock_query_version):
+        mock_query_version.return_value = SAMPLE_UNSUPPORTED_VERSION 
+        try:
+            pg_get_version()
+        except UnsupportedPostgresVersionError, e:
+            self.assertIn('Unsupported', str(e)) 
+            self.assertIn('7.4', str(e)) 
+
+
 @patch('bbpgsql.pg_backup_control.check_output')
 class Test_get_pg_version_output(TestCase):
     def test_calls_psql_version(self, mock_check_output):
@@ -56,6 +78,7 @@ class Test_get_pg_version_output(TestCase):
         mock_check_output.assert_called_with(['psql', '--version'])
 
 
+@patch('bbpgsql.pg_backup_control.pg_get_version')
 @patch('bbpgsql.pg_backup_control.check_output')
 class Test_pg_start_backup(TestCase):
     def setUp(self):
@@ -65,20 +88,37 @@ class Test_pg_start_backup(TestCase):
     def tearDown(self):
         pass
 
-    def test_function_returns_WAL_filename(self, mock_check_output):
+    def test_function_returns_WAL_filename(self, mock_check_output, mock_vers):
+        mock_vers.return_value = '8.4'
         mock_check_output.return_value = self.pgsql_return
         output = pg_start_backup('label')
         self.assertEqual(self.WAL_FILENAME, output)
 
-    def test_check_input_arguments_correct(self, mock_check_output):
+    def test_check_input_arguments_correct_for_8_4(self, mock_check_output,
+            mock_vers):
+        mock_vers.return_value = '8.4'
         cmd_prefix = "SELECT pg_xlogfile_name(pg_start_backup('"
         label = 'label'
         cmd_suffix = "', true));"
         sql_cmd = ''.join([cmd_prefix, label, cmd_suffix])
         pg_start_backup(label)
-        mock_check_output.assert_called_with(['psql', 'postgres', '-t', '-c', sql_cmd])
+        mock_vers.assert_called_once_with()
+        mock_check_output.assert_called_with(['psql', 'postgres', '-t', '-c',
+            sql_cmd])
 
-    def test_wal_name_cleanup(self, mock_check_output):
+    def test_check_input_arguments_correct_for_8_3(self, mock_check_output,
+            mock_vers):
+        mock_vers.return_value = '8.3'
+        cmd_prefix = "SELECT pg_xlogfile_name(pg_start_backup('"
+        label = 'label'
+        cmd_suffix = "'));"
+        sql_cmd = ''.join([cmd_prefix, label, cmd_suffix])
+        pg_start_backup(label)
+        mock_vers.assert_called_once_with()
+        mock_check_output.assert_called_with(['psql', 'postgres', '-t', '-c',
+            sql_cmd])
+
+    def test_wal_name_cleanup(self, mock_check_output, mock_vers):
         output = wal_name_cleanup(self.pgsql_return)
         self.assertEqual(self.WAL_FILENAME, output)
 
