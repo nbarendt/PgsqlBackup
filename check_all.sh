d768f28d153a380f02d1ba44081e2b176e6d70ce
Mark Dohring <markdohring@gmail.com>
2011-05-09 14:42:10 -0400

    added versions of pyflakes find cmds with -print predicate for debug

--- check_all.sh
+++ check_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 find bbpgsql tests -name "*.py" | xargs pyflakes
 find bbpgsql/cmdline_scripts -name "*" | xargs pyflakes
+#find bbpgsql tests -name "*.py" -print -exec pyflakes \{\} \;
+#find bbpgsql/cmdline_scripts -name "*" -print -exec pyflakes \{\} \;
 pep8 bbpgsql/cmdline_scripts/archivewal
 pep8 bbpgsql/cmdline_scripts/archivepgsql
 
